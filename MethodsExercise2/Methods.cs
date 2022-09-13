89b1f27efe03ab8d9d19fd8c74b64c857ab8e642
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-13 09:56:06 -0500

    Add project files.

---
+++ MethodsExercise2/Methods.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise2
+{
+    internal class Methods
+    {
+        public static void Exercise1()
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number>");
+            var favoriteNumber = Console.ReadLine();
+
+            Console.WriteLine($"Hi {name}, nice to meet you.  I just learned that your favorite color is {color}, your " +
+                $"favorite animal is a {animal} and your favorite number is {favoriteNumber}.  " +
+                $"It is fun to learn things about you.");
+        }
+
+        
+        //add, subtract, multiply, divide, modulus
+        public static int Add(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+
+        public static int Sub(int num1, int num2) 
+        {
+            return num1 - num2;
+
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+    
+        public static int BonusParams(params int[] listOfNumbers)
+        {
+            int totalAdd = 0;
+            foreach (int num in listOfNumbers)
+            {
+                totalAdd += num;
+            }
+
+            return totalAdd;
+        }
+    }
+
+
+}
---
+++ MethodsExercise2/Program.cs
@@ -0,0 +1,44 @@
+﻿//Ask the user for the following input.  Turn it into a short story.
+
+using MethodsExercise2;
+using System;
+using System.ComponentModel;
+
+Console.WriteLine("---------------------Step 5c ----------------------");
+//Methods.Exercise1();
+
+
+Console.WriteLine("---------------------- Step 6 ----------------------");
+//add, subtract, multiply, divide, modulus
+
+
+
+Console.Write("Addition answer is:");
+Console.WriteLine(Methods.Add(2, 2));
+
+Console.Write("Subtraction answer is:");
+Console.WriteLine(Methods.Sub(5, 3));
+
+Console.Write("Multiplication answer is:");
+Console.WriteLine(Methods.Multiply(2, 2, 2));
+
+
+Console.Write("Multiplication answer is:");
+Console.WriteLine(Methods.Divide(4, 3));
+
+Console.Write("Modulus answer is:");
+Console.WriteLine(Methods.Modulus(20, 5));
+
+
+Console.WriteLine("---------------------- Step 8 Bonus ----------------------");
+//Change the functions to us the params keyword to support a range of arguements.
+//Add() methhod that takes as many arguements as needed
+
+//a. Sum(2,4) would return 6
+Console.WriteLine(Methods.BonusParams(2,4));
+
+//a. Sum(2,4,6) would return 12
+Console.WriteLine(Methods.BonusParams(2,4,6));
+
+//a. Sum(1,1,1,1,1) would return 5
+Console.WriteLine(Methods.BonusParams(1,1,1,1,1));
